354a33ef4805698a6a6df283192af0a40013ba8e
hooyofaadumo-netizen <hooyofaadumo162@gmail.com>
2026-02-19 21:35:09 +0300

    java ex1

---
+++ java ex.1/main.js
@@ -0,0 +1,23 @@
+let x = 7
+let y = 3
+
+
+console.log(x + y);
+console.log(x - y);
+console.log(x * y)
+console.log(x / y)
+console.log(x % y)
+// post increment
+// plus minus
+console.log(y++);
+console.log(y);
+console.log(y--)
+console.log(y)
+
+
+
+// pre increment
+// plus minus
+console.log(++x)
+console.log(--x)
+
