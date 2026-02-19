fa486ff865e7350f97b5f283510d9fa0d850d00b
hooyofaadumo-netizen <hooyofaadumo162@gmail.com>
2026-02-19 23:05:38 +0300

    adding js.ex2

---
+++ java ex.2/main.js
@@ -0,0 +1,11 @@
+let a = 18
+let b = "18"
+
+console.log(a == b)
+console.log(a != b)
+console.log(a === b)
+console.log(a !== b)
+console.log(a > b)
+console.log(a < b)
+console.log(a >= b)
+console.log(a <= b)
