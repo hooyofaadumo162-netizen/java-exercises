8c0d964cd6ebcbc20aee1a38b851a238c98a6373
hooyofaadumo-netizen <hooyofaadumo162@gmail.com>
2026-02-24 19:34:18 +0300

    jave ex.4

---
+++ java ex.4/main.js
@@ -0,0 +1,28 @@
+
+
+// return function
+
+function add(q,w) {
+    return q+w
+}
+
+let result = add(20,15) 
+console.log(20+15);
+console.log(20*15)
+console.log(20/15)
+console.log(20-15)
+
+
+// expression function
+const win = function() {
+    console.log("am winner")
+}
+win()
+
+
+
+let foot = function(foot) {
+    console.log("am winner " + foot)
+}
+foot("my love rice")
+
