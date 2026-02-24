8910142f7a71820c43a772b236569462717accf8
hooyofaadumo-netizen <hooyofaadumo162@gmail.com>
2026-02-24 20:53:49 +0300

    java ex.6

---
+++ java ex.6/main.js
@@ -0,0 +1,21 @@
+
+
+// arrays 
+
+let colors = ["Pink", "Black", "White", "parple", "brown", "yellow"]
+console.log(colors)
+console.log(colors[4])
+console.log(colors[0])
+console.log(colors[3])
+console.log(colors[1])
+console.log(colors[5])
+console.log(colors[2])
+
+
+
+console.log(colors[0])
+console.log(colors[1])
+console.log(colors[2])
+console.log(colors[3])
+console.log(colors[4])
+console.log(colors[5])
