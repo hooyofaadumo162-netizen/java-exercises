ec22f1929c85ee7e7885405bf9ca84364fcd5801
hooyofaadumo-netizen <hooyofaadumo162@gmail.com>
2026-02-25 21:12:40 +0300

    java ex.7

---
+++ java ex.7/main.js
@@ -0,0 +1,56 @@
+
+// array methods
+const colors = ["Pink", "Black", "White", "parple", "brown", "yellow"]
+console.log(colors)
+console.log(colors[0])
+console.log(colors[1])
+console.log(colors[2])
+console.log(colors[3])
+console.log(colors[4])
+console.log(colors[5])
+
+// show me the elemts inside of an arrays
+console.log(colors.length)
+
+// push added element to the end of an array
+colors.push("red")
+console.log(colors)
+
+// show me the elements of an arrays after push added the last  element
+console.log(colors.length)
+
+
+// It removes the end element of the array
+colors.pop("red")
+console.log(colors)
+
+// show me the elements of an arrays after pop removes the last element
+console.log(colors.length)
+
+
+// adds elemet to the beginning of an array
+colors.unshift("green")
+console.log(colors)
+
+// show me the elements of an arrays after unshift added the firs element
+console.log(colors.length)
+
+
+
+// It removes the begnning element of the array
+colors.shift("green")
+console.log(colors)
+
+
+// Show me the elements of an array after shift removes the first element
+console.log(colors.length)
+
+
+
+
+
+colors.push("violote")
+console.log(colors)
+
+colors.unshift("silver")
+console.log(colors)
