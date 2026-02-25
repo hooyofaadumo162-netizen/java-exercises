1574ec28e917016ab0cb56de3f3710e875808c3c
hooyofaadumo-netizen <hooyofaadumo162@gmail.com>
2026-02-25 23:20:51 +0300

    java ex.8

---
+++ java ex.8/main.js
@@ -0,0 +1,18 @@
+
+
+
+let car = {
+    model:"Mercedes-Benz",
+    make:1901,
+    year:1901,
+};
+
+console.log(car.model);
+console.log(car.make);
+console.log(car.year);
+
+
+// as array
+console.log(car["make"]);
+console.log(car["year"]);
+console.log(car["model"]);
