57783fa08a752037acc60461439f6bdeccdcf982
hooyofaadumo-netizen <hooyofaadumo162@gmail.com>
2026-03-07 22:58:21 +0300

    filasha waxa ka badbadalay ex.10 iyo ex.11 Aakusoo daray

---
+++ java ex.10/main.js
@@ -0,0 +1,23 @@
+
+
+
+
+// array object 
+
+ let library = [
+   {
+    title: "Zikola", 
+    author: "Amar abdil hamid", 
+    year: (2010)
+},
+
+{
+    title: "Aazar", 
+    author: "Ahmed Aal hamdan", 
+    year: (2025)
+}
+ ]
+    
+
+console.log(library[0].title)
+console.log(library[1].author)
---
+++ java ex.11/main.js
@@ -0,0 +1,15 @@
+
+
+
+
+// for loop 1-50
+
+for(let i=1; i < 50; i++){
+    console.log(i)
+}
+
+
+
+for(let y=2; y < 12; y++){
+    console.log(y)
+}
