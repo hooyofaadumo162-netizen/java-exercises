c36b0aa9ca6e9ce5ed268abca4f4bfa5266a6f33
hooyofaadumo-netizen <hooyofaadumo162@gmail.com>
2026-03-18 21:48:40 +0300

    java ex.14

---
+++ java ex.14/main.js
@@ -0,0 +1,38 @@
+
+
+
+
+
+
+
+
+
+//  do while loop
+
+let i = 10
+do {
+    console.log(i), i++
+}while( i < 5);
+
+
+
+
+
+let password;
+do {
+  password = prompt ("enter your password")
+}while(password !== "0618692727")
+
+    console.log("welcome Eng")
+
+
+
+
+    let  name;
+
+    do {
+        name = prompt ("inter your name")
+    }while(name !== "Maria")
+
+
+        console.log("maria")
