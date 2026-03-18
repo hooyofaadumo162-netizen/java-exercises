d95db587d1b50c83df644ef38d973bfdee601d5a
hooyofaadumo-netizen <hooyofaadumo162@gmail.com>
2026-03-19 00:05:58 +0300

    java ex.16

---
+++ java ex.16/main.js
@@ -0,0 +1,21 @@
+
+
+// conditions
+
+// if statement 
+
+ let age = 18;
+ if (age >= 18) {
+    console.log("you are an adult")
+ }
+
+//  if - else statement
+
+ let Age = 17
+ if (Age <= 17) {
+    console.log("you are a minor")
+ }else{
+    console.log("you are a minor")
+ }
+
+ 
