020cc562e064307f3e2117b61b17d25314b884a8
hooyofaadumo-netizen <hooyofaadumo162@gmail.com>
2026-03-22 20:51:01 +0300

    java ex.17

---
+++ java ex.17/main.js
@@ -0,0 +1,20 @@
+
+
+
+
+
+
+// else if statement
+
+
+let temparator = 0
+
+if(temparator <= 0){
+    console.log( "Very Cold")
+}else if(temparator < 15){
+    console.log("cold")
+}else if(temparator < 25){
+    console.log("warm")
+}else if(temparator > 25){
+    console.log("hot")
+}
