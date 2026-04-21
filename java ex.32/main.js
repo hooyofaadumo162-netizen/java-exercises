d6149231aa1d68a4272c1e22def0d95e834ef56b
hooyofaadumo-netizen <hooyofaadumo162@gmail.com>
2026-04-21 10:34:14 +0300

    java ex.32

---
+++ java ex.32/main.js
@@ -0,0 +1,20 @@
+
+
+
+
+const mara = document.querySelector('#mara')
+console.log(mara)
+
+
+const prag = document.querySelectorAll('.prag')
+console.log(prag)
+
+
+const button = document.querySelectorAll('button')[0]
+console.log(button.outerHTML)
+
+
+const div = document.querySelectorAll('div')[1]
+console.log(div.textContent)
+
+
