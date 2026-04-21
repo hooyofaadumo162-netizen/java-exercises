0b0f30a9ee0ca8ae38f473850fdb4a88411bc41d
hooyofaadumo-netizen <hooyofaadumo162@gmail.com>
2026-04-21 11:24:06 +0300

    java ex.33

---
+++ java ex.33/main.js
@@ -0,0 +1,14 @@
+
+
+
+// changing content
+
+
+function elementBadalid(){
+    header.textContent = ("JAVA ASSIGNMENT #33")
+}
+
+
+function textBadalid(){
+    h2.innerHTML = `before <strong >YOU REGET </strong`
+}
